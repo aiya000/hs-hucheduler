d30f04c3592a10b3f55b620dfd894ee14003e489
aiya000 <aiya000.develop@gmail.com>
2017-04-22 15:14:05 +0900

    Implement TextShow instances

--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Control.Exception.Safe (SomeException, Exception, throwM, MonadCatch)
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad.Trans.Either (runEitherT)
 import Data.Default (Default, def)
+import Data.List (foldl1')
+import Data.Monoid ((<>))
 import Data.Text (Text)
 import Safe (readMay)
 import System.EasyFile (doesFileExist)
 import System.Environment (getEnv)
+import TextShow (TextShow, showb, showt)
+import qualified Data.Text.IO as TIO
+import qualified TextShow as TS
 
 data ScheduleOfDay = ScheduleOfDay [Text]
   deriving (Show, Read)
+instance TextShow ScheduleOfDay where
+  showb (ScheduleOfDay []) = TS.fromText ""
+  showb (ScheduleOfDay xs) = TS.fromText $ foldl1' encloseSpaceCon xs
+    where
+      encloseSpaceCon :: Text -> Text -> Text
+      encloseSpaceCon x y = x <> ", " <> y
 
 -- | A schedule of day of week
 data Schedules = Schedules
@@ -24,6 +37,15 @@ data Schedules = Schedules
   } deriving (Show, Read)
 instance Default Schedules where
   def = emptySchedules
+instance TextShow Schedules where
+  showb (Schedules mon thue wed thur fri sat sun) =
+    TS.fromText $ "月: " <> showt mon  <>  "\n" <>
+                  "火: " <> showt thue <>  "\n" <>
+                  "水: " <> showt wed  <>  "\n" <>
+                  "木: " <> showt thur <>  "\n" <>
+                  "金: " <> showt fri  <>  "\n" <>
+                  "土: " <> showt sat  <>  "\n" <>
+                  "日: " <> showt sun
 
 data ConfigurationException = ConfigurationException String
 instance Exception ConfigurationException
@@ -48,7 +70,7 @@ main = do
   schedulesOrNot <- runEitherT readHuchedulerConfig
   case schedulesOrNot of
     Left  e         -> putStrLn $ show (e :: SomeException)
-    Right schedules -> print schedules
+    Right schedules -> TIO.putStrLn $ showt schedules
 
 
 readHuchedulerConfig :: (MonadIO m, MonadCatch m) => m Schedules
